7d47be441f8e83a641a72d20eb349d71827e3cd2
shirakomamu <mamu@shirako.dev>
2021-06-16 20:47:14 -0700

    remove error put for testing

--- server/src/routes/auth/methods/isNameAvailable.ts
+++ server/src/routes/auth/methods/isNameAvailable.ts
@@ -2,7 +2,6 @@ import { SrkCookie } from "src/services/jwt";
 import { NameCheckDto } from "@@/common/dto/auth";
 import { INameCheckPayload } from "@@/common/interfaces/api";
 import { DI } from "src/app";
-import SrkError from "src/classes/SrkError";
 import getAvailableDiscriminators from "./utils/getAvailableDiscriminators";
 
 export default async function (
@@ -15,7 +14,6 @@ export default async function (
       displayName: name,
     });
     isAvailable = availDiscrims.length > 0;
-    throw new SrkError("displayNameNotAvailable");
   } else {
     const user = await DI.memberRepo.findOne({
       username: name,
