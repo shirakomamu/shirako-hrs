9faa733c1146c00fe64790290ef9650db7844421
shirakomamu <mamu@shirako.dev>
2021-07-27 22:37:43 -0700

    change description to be consistent

--- appinfo.ts
+++ appinfo.ts
@@ -1,7 +1,7 @@
 export default {
   name: "Shirako Eats",
   description:
-    "It's a new way to eat. Just tell us which restaurants you like, and we'll do the rest.",
+    "It's a new way to eat. Just tell us which restaurants you like, and we'll pick for you.",
   favicon: "/favicon.png",
   version: process.env.npm_package_version || "unknown",
 };
