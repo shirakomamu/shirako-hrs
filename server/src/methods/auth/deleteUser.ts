69859955107f3881880d2cfb086daf922ec23df1
shirakomamu <mamu@shirako.dev>
2021-08-06 20:18:16 -0700

    fix user deleting

--- server/src/methods/auth/deleteUser.ts
+++ server/src/methods/auth/deleteUser.ts
@@ -5,22 +5,28 @@ import { DI } from "server/middleware/initializeDi";
 import sendgrid from "server/services/sendgrid";
 import hrbacCan from "common/utils/hrbacCan";
 import { Role } from "common/enums/hrbac";
+import { clearCache } from "server/services/auth0-mgmt/getUserCached";
 
 export default async (authResult: SrkCookie): Promise<void> => {
   if (!authResult.actor) {
     throw new SrkError("unauthorized");
   }
 
-  const self = await DI.memberRepo.findOneOrFail({ sub: authResult.actor.id });
+  const self = await DI.memberRepo.findOneOrFail({ sub: authResult.actor.id }, [
+    "apiKey",
+    "destinationLists",
+    "outgoingFriends",
+    "incomingFriends",
+  ]);
 
-  await DI.em.begin();
-  try {
-    DI.em.remove(self);
-    await deleteUser(authResult.actor.id);
-    DI.em.commit();
-  } catch (e) {
-    DI.em.rollback();
-  }
+  // await DI.em.begin();
+  // try {
+  await DI.em.removeAndFlush(self);
+  await deleteUser(authResult.actor.id);
+  // await DI.em.commit();
+  // } catch (e) {
+  // DI.em.rollback();
+  // }
 
   if (hrbacCan({ roles: [Role._email_verified] }, authResult.actor)) {
     await sendgrid.sendAccountDeletionConfirmation({
@@ -28,4 +34,8 @@ export default async (authResult: SrkCookie): Promise<void> => {
       name: authResult.actor.username,
     });
   }
+
+  // clear caches
+  clearCache({ id: authResult.actor.id });
+  clearCache({ username: authResult.actor.username });
 };
